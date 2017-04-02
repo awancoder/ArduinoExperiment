50d772744d893a433f729722579b8323da956217
Awan <awancoder@gmail.com>
2017-04-03 06:47:36 +0700

    Add files via upload

---
+++ LDR/LDR.ino
@@ -0,0 +1,19 @@
+const int ldrPin = A0;
+const int ldrPin2 = A1;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ldrPin, INPUT);
+  pinMode(ldrPin2, INPUT);
+}
+
+void loop() {
+  int ldrStatus = analogRead(ldrPin);
+  int ldrStatus2 = analogRead(ldrPin2);
+ 
+  Serial.print("LDR 1: ");
+  Serial.println(ldrStatus);
+  Serial.print("LDR 2: ");
+  Serial.println(ldrStatus2);  
+  delay(250); 
+}
---
+++ Servo/Servo.ino
@@ -0,0 +1,30 @@
+#include <Servo.h> 
+Servo myservo;
+int pos = 0;            
+void setup() 
+{
+  myservo.attach(9);
+}
+void loop() 
+{
+  
+// // start dari 0 derajar sampai 180 derajat 
+// for(pos = 0; pos < 180; pos += 1)  
+// {
+//  // pada posisi 1 derajat
+//  // memberitahu servo untuk pergi ke posisi  'pos'
+//  myservo.write(pos);
+//  // tunggu 15ms untuk pencapaian  posisi servo    
+//  delay(15);                  
+// }
+// 
+// // start dari 180 derajat ke 0 derajat 
+// for(pos = 180; pos>=1; pos-=1)  
+// {
+//  // memberitahu servo untuk pergi ke posisi  'pos'                                
+//  myservo.write(pos);                 
+//  // tunggu 15ms untuk pencapaian  posisi servo    
+//  delay(15);                        
+// }
+myservo.write(15); 
+}
