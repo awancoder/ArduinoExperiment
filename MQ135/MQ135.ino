586e46a84ee0329813217ae7acaff5d7dbeb3cd5
Awan <awancoder@gmail.com>
2017-04-05 07:19:26 +0700

    Add files via upload

---
+++ MQ135/MQ135.ino
@@ -0,0 +1,14 @@
+int sensorValue;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(0); // read analog input pin 0
+  Serial.print("Air Quality: ");
+  Serial.print(sensorValue, DEC);  // prints the value read
+  Serial.println(" ppm");
+  delay(1000);
+}
